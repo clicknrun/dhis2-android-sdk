7e7c50af107dad7e86dd6a91890fec05a5e1e142
Marcos Campos <marcamsn@gmail.com>
2017-11-23 12:09:11 +0100

    datasets: Add data set model

---
+++ core/src/main/java/org/hisp/dhis/android/core/dataset/DataSetModel.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2017, University of Oslo
+ *
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.dataset;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.gabrielittner.auto.value.cursor.ColumnName;
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.core.common.BaseNameableObjectModel;
+
+@AutoValue
+public abstract class DataSetModel extends BaseNameableObjectModel {
+
+    public static final String TABLE = "DataSet";
+
+    public static class Columns extends BaseNameableObjectModel.Columns {
+        public static final String PERIOD_TYPE = "periodType";
+        public static final String CATEGORY_COMBO = "categoryCombo";
+        public static final String MOBILE = "mobile";
+        public static final String VERSION = "version";
+        public static final String EXPIRY_DAYS = "expiryDays";
+        public static final String TIMELY_DAYS = "timelyDays";
+        public static final String NOTIFY_COMPLETING_USER = "notifyCompletingUser";
+        public static final String OPEN_FUTURE_PERIODS = "openFuturePeriods";
+        public static final String FIELD_COMBINATION_REQUIRED = "fieldCombinationRequired";
+        public static final String VALID_COMPLETE_ONLY = "validCompleteOnly";
+        public static final String NO_VALUE_REQUIRES_COMMENT = "noValueRequiresComment";
+        public static final String SKIP_OFFLINE = "skipOffline";
+        public static final String DATA_ELEMENT_DECORATION = "dataElementDecoration";
+        public static final String RENDER_AS_TABS = "renderAsTabs";
+        public static final String RENDER_HORIZONTALLY = "renderHorizontally";
+    }
+
+    public static DataSetModel create(Cursor cursor) {
+        return AutoValue_DataSetModel.createFromCursor(cursor);
+    }
+
+    public static Builder builder() {
+        return new $$AutoValue_DataSetModel.Builder();
+    }
+
+    /* TODO Create PeriodType
+    @Nullable
+    @ColumnName(Columns.PERIOD_TYPE)
+    @ColumnAdapter(DbPeriodTypeColumnAdapter.class)
+    public abstract PeriodType periodType();*/
+
+    @Nullable
+    @ColumnName(Columns.PERIOD_TYPE)
+    public abstract String periodType();
+
+    @Nullable
+    @ColumnName(Columns.CATEGORY_COMBO)
+    public abstract String categoryCombo();
+
+    @Nullable
+    @ColumnName(Columns.MOBILE)
+    public abstract Boolean mobile();
+
+    @Nullable
+    @ColumnName(Columns.VERSION)
+    public abstract Integer version();
+
+    @Nullable
+    @ColumnName(Columns.EXPIRY_DAYS)
+    public abstract Integer expiryDays();
+
+    @Nullable
+    @ColumnName(Columns.TIMELY_DAYS)
+    public abstract Integer timelyDays();
+
+    @Nullable
+    @ColumnName(Columns.NOTIFY_COMPLETING_USER)
+    public abstract Boolean notifyCompletingUser();
+
+    @Nullable
+    @ColumnName(Columns.OPEN_FUTURE_PERIODS)
+    public abstract Integer openFuturePeriods();
+
+    @Nullable
+    @ColumnName(Columns.FIELD_COMBINATION_REQUIRED)
+    public abstract Boolean fieldCombinationRequired();
+
+    @Nullable
+    @ColumnName(Columns.VALID_COMPLETE_ONLY)
+    public abstract Boolean validCompleteOnly();
+
+    @Nullable
+    @ColumnName(Columns.NO_VALUE_REQUIRES_COMMENT)
+    public abstract Boolean noValueRequiresComment();
+
+    @Nullable
+    @ColumnName(Columns.SKIP_OFFLINE)
+    public abstract Boolean skipOffline();
+
+    @Nullable
+    @ColumnName(Columns.DATA_ELEMENT_DECORATION)
+    public abstract Boolean dataElementDecoration();
+
+    @Nullable
+    @ColumnName(Columns.RENDER_AS_TABS)
+    public abstract Boolean renderAsTabs();
+
+    @Nullable
+    @ColumnName(Columns.RENDER_HORIZONTALLY)
+    public abstract Boolean renderHorizontally();
+
+    @NonNull
+    public abstract ContentValues toContentValues();
+
+    @AutoValue.Builder
+    public static abstract class Builder extends BaseNameableObjectModel.Builder<Builder> {
+        public abstract Builder periodType(String periodType);
+
+        public abstract Builder categoryCombo(String categoryCombo);
+
+        public abstract Builder mobile(Boolean mobile);
+
+        public abstract Builder version(Integer version);
+
+        public abstract Builder expiryDays(Integer expiryDays);
+
+        public abstract Builder timelyDays(Integer timelyDays);
+
+        public abstract Builder notifyCompletingUser(Boolean notifyCompletingUser);
+
+        public abstract Builder openFuturePeriods(Integer openFuturePeriods);
+
+        public abstract Builder fieldCombinationRequired(Boolean fieldCombinationRequired);
+
+        public abstract Builder validCompleteOnly(Boolean validCompleteOnly);
+
+        public abstract Builder noValueRequiresComment(Boolean noValueRequiresComment);
+
+        public abstract Builder skipOffline(Boolean skipOffline);
+
+        public abstract Builder dataElementDecoration(Boolean dataElementDecoration);
+
+        public abstract Builder renderAsTabs(Boolean renderAsTabs);
+
+        public abstract Builder renderHorizontally(Boolean renderHorizontally);
+
+        public abstract DataSetModel build();
+    }
+}
