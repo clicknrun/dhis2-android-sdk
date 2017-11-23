bfeea089e281dbf0ef8b5e852a1c223e7c3424f9
Marcos Campos <marcamsn@gmail.com>
2017-11-23 15:50:09 +0100

    datasets: delete imports and comment in pojo

--- core/src/main/java/org/hisp/dhis/android/core/dataset/DataSet.java
+++ core/src/main/java/org/hisp/dhis/android/core/dataset/DataSet.java
@@ -35,10 +35,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 
 import org.hisp.dhis.android.core.common.BaseNameableObject;
-import org.hisp.dhis.android.core.common.ValueType;
 import org.hisp.dhis.android.core.data.api.Field;
-import org.hisp.dhis.android.core.data.api.NestedField;
-import org.hisp.dhis.android.core.option.OptionSet;
 
 import java.util.Date;
 
@@ -88,35 +85,6 @@ public abstract class DataSet extends BaseNameableObject {
     public static final Field<DataSet, Boolean> dataElementDecoration = Field.create(DATA_ELEMENT_DECORATION);
     public static final Field<DataSet, Boolean> renderAsTabs = Field.create(RENDER_AS_TABS);
     public static final Field<DataSet, Boolean> renderHorizontally = Field.create(RENDER_HORIZONTALLY);
-
-
-/*
-    @JsonProperty(UID) String uid,
-    @JsonProperty(CODE) String code,
-    @JsonProperty(NAME) String name,
-    @JsonProperty(DISPLAY_NAME) String displayName,
-    @JsonProperty(CREATED) Date created,
-    @JsonProperty(LAST_UPDATED) Date lastUpdated,
-    @JsonProperty(SHORT_NAME) String shortName,
-    @JsonProperty(DISPLAY_SHORT_NAME) String displayShortName,
-    @JsonProperty(DESCRIPTION) String description,
-    @JsonProperty(DISPLAY_DESCRIPTION) String displayDescription,
-    @JsonProperty(PERIOD_TYPE) String periodType,
-    @JsonProperty(CATEGORY_COMBO) String categoryCombo,
-    @JsonProperty(MOBILE) Boolean mobile,
-    @JsonProperty(VERSION) Integer version,
-    @JsonProperty(EXPIRY_DAYS) Integer expiryDays,
-    @JsonProperty(TIMELY_DAYS) Integer timelyDays,
-    @JsonProperty(NOTIFY_COMPLETING_USER) Boolean notifyCompletingUser,
-    @JsonProperty(OPEN_FUTURE_PERIODS) Integer openFuturePeriods,
-    @JsonProperty(FIELD_COMBINATION_REQUIRED) Boolean fieldCombinationRequired,
-    @JsonProperty(VALID_COMPLETE_ONLY) Boolean validCompleteOnly,
-    @JsonProperty(NO_VALUE_REQUIRES_COMMENT) Boolean noValueRequiresComment,
-    @JsonProperty(SKIP_OFFLINE) Boolean skipOffline,
-    @JsonProperty(DATA_ELEMENT_DECORATION) Boolean dataElementDecoration,
-    @JsonProperty(RENDER_AS_TABS) Boolean renderAsTabs,
-    @JsonProperty(RENDER_HORIZONTALLY) Boolean renderHorizontally,
-    @JsonProperty(DELETED) Boolean deleted) {*/
 
     /* TODO Change String to PeriodType
     @Nullable
