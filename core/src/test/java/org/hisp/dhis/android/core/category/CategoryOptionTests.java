ebcbd4126e8a977f88650e2efb87a26e0698638c
Marcos Campos <marcamsn@gmail.com>
2017-11-29 09:17:52 +0100

    datasets: adapt categoryOptionTest

--- core/src/test/java/org/hisp/dhis/android/core/category/CategoryOptionTests.java
+++ core/src/test/java/org/hisp/dhis/android/core/category/CategoryOptionTests.java
@@ -35,6 +35,7 @@ import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -44,51 +45,45 @@ public class CategoryOptionTests {
     @Test
     public void categoryOption_shouldMapFromJsonString() throws IOException, ParseException {
         ObjectMapper objectMapper = Inject.objectMapper();
-        CategoryOption option = objectMapper.readValue("{" +
-                        "\"lastUpdated\":\"2016-08-08T11:17:59.448\"," +
-                        "\"id\":\"cQYFfHX9oIT\"," +
-                        "\"created\":\"2016-08-08T11:17:59.448\"," +
-                        "\"name\":\"Green\"," +
-                        "\"shortName\":\"Green\"," +
-                        "\"displayName\":\"Green\"," +
-                        "\"displayShortName\":\"Green\"," +
-                        "\"startDate\":\"2016-04-01T00:00:00.000\"," +
-                        "\"endDate\":\"2016-05-01T00:00:00.000\"," +
-                        "\"externalAccess\":false," +
-                        "\"dimensionItem\":\"cQYFfHX9oIT\"," +
-                        "\"dimensionItemType\":\"CATEGORY_OPTION\"," +
-                        "\"categories\":[]," +
-                        "\"organisationUnits\":[]," +
-                        "\"categoryOptionCombos\":[" +
-                        "{\"id\":\"S34ULMcHMca\"}," +
-                        "{\"id\":\"sqGRzCziswD\"}," +
-                        "{\"id\":\"WVzTbHctjok\"}," +
-                        "{\"id\":\"QFcGyiRFFH5\"}" +
-                        "]," +
-                        "\"categoryOptionGroups\":[]" +
-                        "}",
-                CategoryOption.class);
+        InputStream jsonStream = this.getClass().getClassLoader()
+                .getResourceAsStream("category/categoryOption.json");
 
-        assertThat(option.uid()).isEqualTo("cQYFfHX9oIT");
+        CategoryOption option = objectMapper.readValue(jsonStream, CategoryOption.class);
+
+
+        assertThat(option.uid()).isEqualTo("i4Nbp8S2G6A");
         assertThat(option.created()).isEqualTo(
-                BaseIdentifiableObject.DATE_FORMAT.parse("2016-08-08T11:17:59.448"));
+                BaseIdentifiableObject.DATE_FORMAT.parse("2013-12-20T22:14:03.322"));
         assertThat(option.lastUpdated()).isEqualTo(
-                BaseIdentifiableObject.DATE_FORMAT.parse("2016-08-08T11:17:59.448"));
+                BaseIdentifiableObject.DATE_FORMAT.parse("2016-07-04T18:26:53.030"));
 
-        assertThat(option.name()).isEqualTo("Green");
-        assertThat(option.shortName()).isEqualTo("Green");
-        assertThat(option.displayName()).isEqualTo("Green");
-        assertThat(option.displayShortName()).isEqualTo("Green");
+        assertThat(option.name()).isEqualTo("Improve access to clean water");
+        assertThat(option.shortName()).isEqualTo("Improve access to clean water");
+        assertThat(option.displayName()).isEqualTo("Improve access to clean water");
+        assertThat(option.displayShortName()).isEqualTo("Improve access to clean water");
 
         assertThat(option.startDate()).isEqualTo(
                 BaseIdentifiableObject.DATE_FORMAT.parse("2016-04-01T00:00:00.000"));
         assertThat(option.endDate()).isEqualTo(
-                BaseIdentifiableObject.DATE_FORMAT.parse("2016-05-01T00:00:00.000"));
+                BaseIdentifiableObject.DATE_FORMAT.parse("2018-08-01T00:00:00.000"));
 
         // check if list maintains order of the items in payload
-        assertThat(option.categoryOptionCombos().get(0).uid()).isEqualTo("S34ULMcHMca");
-        assertThat(option.categoryOptionCombos().get(1).uid()).isEqualTo("sqGRzCziswD");
-        assertThat(option.categoryOptionCombos().get(2).uid()).isEqualTo("WVzTbHctjok");
-        assertThat(option.categoryOptionCombos().get(3).uid()).isEqualTo("QFcGyiRFFH5");
+        assertThat(option.categoryOptionCombos().get(0).uid()).isEqualTo("SeWJkpLAyLt");
+        assertThat(option.categoryOptionCombos().get(1).uid()).isEqualTo("CQMZckgv9d2");
+        assertThat(option.categoryOptionCombos().get(2).uid()).isEqualTo("fC3z1lcAW5x");
+        assertThat(option.categoryOptionCombos().get(3).uid()).isEqualTo("N7QFN41eTN8");
+        assertThat(option.categoryOptionCombos().get(4).uid()).isEqualTo("XVl0bL5Bl4q");
+        assertThat(option.categoryOptionCombos().get(5).uid()).isEqualTo("h5i1N1wcq9G");
+        assertThat(option.categoryOptionCombos().get(6).uid()).isEqualTo("pO5CEqK6c1s");
+        assertThat(option.categoryOptionCombos().get(7).uid()).isEqualTo("M0PqoB8pblq");
+        assertThat(option.categoryOptionCombos().get(8).uid()).isEqualTo("KnS3rABktqN");
+        assertThat(option.categoryOptionCombos().get(9).uid()).isEqualTo("i3HQ1ziPjC7");
+        assertThat(option.categoryOptionCombos().get(10).uid()).isEqualTo("RRJ9WsqSrVs");
+        assertThat(option.categoryOptionCombos().get(11).uid()).isEqualTo("BN9KwUloeSL");
+        assertThat(option.categoryOptionCombos().get(12).uid()).isEqualTo("E29j0LFxvSV");
+        assertThat(option.categoryOptionCombos().get(13).uid()).isEqualTo("QjyqqJMm0X7");
+        assertThat(option.categoryOptionCombos().get(14).uid()).isEqualTo("RuYW1OENYhV");
+        assertThat(option.categoryOptionCombos().get(15).uid()).isEqualTo("cUb8seKgn2y");
+        assertThat(option.categoryOptionCombos().get(16).uid()).isEqualTo("GZMZ9gZMvGs");
     }
 }
