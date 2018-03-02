b65f66ff2c1e772f5c4fec4fcc413a354fd64766
Marcos Campos <marcamsn@gmail.com>
2018-03-02 15:03:34 +0100

    the-merge: compile DataValueQuery

--- core/src/main/java/org/hisp/dhis/android/core/datavalue/DataValueQuery.java
+++ core/src/main/java/org/hisp/dhis/android/core/datavalue/DataValueQuery.java
@@ -15,7 +15,7 @@ public abstract class DataValueQuery extends BaseQuery {
     public abstract Set<String> orgUnitUids();
 
     public static DataValueQuery create(Set<String> dataSetUids, Set<String> periodIds, Set<String> orgUnitUids) {
-        return new AutoValue_DataValueQuery(1, BaseQuery.DEFAULT_PAGE_SIZE, false,
-                dataSetUids, periodIds, orgUnitUids);
+        return new AutoValue_DataValueQuery(null, 1, BaseQuery.DEFAULT_PAGE_SIZE, BaseQuery.DEFAULT_IS_PAGING,
+                BaseQuery.DEFAULT_IS_TRANSLATION_ON, BaseQuery.DEFAULT_TRANSLATION_LOCALE,);
     }
 }
